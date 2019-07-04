80150479ef4f7f2ab0c4a02289e981a00069db36
Joao Santana <joavitorsantana089@gmail.com>
2019-07-04 13:59:50 -0300

    teste 3

--- src/proejto/primeiraClasse.java
+++ src/proejto/primeiraClasse.java
@@ -5,7 +5,7 @@ public class primeiraClasse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("Testando");
+		System.out.print("Teste 3");
 
 	}
 
